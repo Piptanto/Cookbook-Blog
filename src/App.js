ea98b61d265c217484ecfeb262b86e1090659994
alanmiste <baranalaan@gmail.com>
2021-11-13 16:41:03 +0100

    add update item function

--- src/App.js
+++ src/App.js
@@ -11,9 +11,36 @@ function App() {
   
   const loadEntries = async () => {
     const response = await client.getEntries();
-    console.log(response.items);
+    console.log(response.items[1].fields.name);
     setArticles(response.items);
   }
+
+
+  // update an item in a Recipe
+  const contentful = require('contentful-management')
+
+  async function Connect(){ 
+    let client = await contentful.createClient({
+      accessToken:  'CFPAT-cevXEVat_mA6Rr5j6Fvv5KVqeATbc99Gee87egSdsgU'
+      // accessToken:  process.env.REACT_APP_ACCESS_TOKEN
+    })
+    let space = await client.getSpace('gt4lfw53kejq');
+    return await space.getEnvironment('master');
+  }
+
+  async function updateCard(env, cardID){
+    let item = await env.getEntry(cardID)
+    //console.log(item.fields.name['en-US'])
+    item.fields.name['en-US'] = 'Insalata Caprese';
+    await item.update();
+    item = await env.getEntry(cardID);
+    await item.publish();
+  }
+
+  (async () => {
+    let env = await Connect();
+    await updateCard(env, '5VYaJdA8FsLTO3PWJyaYHj')
+  })();
 
   return (
     <div className="App">
