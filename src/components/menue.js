864a632aea7cfe0629ccc51c46bd19f3908d81ba
IreneCoded <codirene.berlin@gmail.com>
2021-11-20 17:47:13 +0100

    Mobile menu started and media queries

--- src/components/menue.js
+++ src/components/menue.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../styles/menu1.css';
+import { IoIosMenu } from "react-icons/io";
+
+
 
 export default function Menu(){
+    const [windowDimension, setWindowDimension] = useState(null);
+    const [mNavClosed, setNavClosed] = useState(true);
 
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log(windowDimension);
+  const handleClick = () =>{
+    mNavClosed? setNavClosed(false):setNavClosed(true)
+  }
     return(
         <nav>
-        <menu>
+           <div> <IoIosMenu size ="70" color ="rgb(226, 121, 0)" className="mobile" onClick="handleClick"/></div>
+        <menu className= {mNavClosed? "noned": "blocked"}>
                 <li>Home</li>
                 <li>Breakfast</li>
                 <li>Lunch</li>
